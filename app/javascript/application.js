7b72f836052f0885a5861cbaf7de7d1615830696
fargelus <ddraudred@gmail.com>
2022-08-31 18:44:53 +0300

    Daterangepicker plugin

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,17 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
 import "@hotwired/turbo-rails"
 import "controllers"
 import 'bootstrap'
+import Rails from '@rails/ujs'
+import 'moment'
+import 'daterangepicker'
+import jquery from 'jquery'
+
+window.$ = jquery
+
+$('.datetimerange').daterangepicker({
+    timePicker: true
+})
+
+Rails.start()
--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,7 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.0/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/lib/index.js"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.0.3-1/lib/assets/compiled/rails-ujs.js"
+pin "daterangepicker", to: "https://ga.jspm.io/npm:daterangepicker@3.1.0/daterangepicker.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
+pin "moment", to: "https://ga.jspm.io/npm:moment@2.29.4/moment.js"
